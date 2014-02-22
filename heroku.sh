3570f6551b90d60eb68fe08f286a1c28111a7e32
Ben Immanuel <immanuel_ben@yahoo.co.uk>
2014-02-22 13:39:28 +0000

    minor change

--- heroku.sh
+++ heroku.sh
@@ -3,3 +3,12 @@ heroku config:set BUILDPACK_URL=https://github.com/heroku/heroku-buildpack-scala
 
 # Unset buildpack
 heroku config:unset BUILDPACK_URL;
+
+# Set multiple buildpacks
+heroku config:set BUILDPACK_URL=https://github.com/ddollar/heroku-buildpack-multi.git;
+
+
+
+https://github.com/heroku/heroku-buildpack-scala
+
+https://github.com/heroku/heroku-buildpack-nodejs
